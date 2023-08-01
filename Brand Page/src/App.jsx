d62ec63db9867284b26c606478f68d24a8a2b20a
RohitNK9 <naikrohit0009@gmail.com>
2023-08-02 05:05:46 +0530

    first commit

---
+++ Brand Page/src/App.jsx
@@ -0,0 +1,17 @@
+import Navigation from './components/Navigation/Navigation'
+import Hero from './components/Hero/Hero'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Navigation />
+      <Hero />
+    </>
+  )
+}
+
+export default App
+
+
